082ce3bdc79d469c69a9e8a2e3d675ab48846793
iambibek-1 <iambibek100@gmail.com>
2024-10-27 12:26:54 +0545

    added testimonaials section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Gallery from "./components/Gallery/Gallery";
 import Title from "./components/Title/Title";
+import Testimonials from "./components/Testimonial/Testimonials";
+import Members from "./components/Members/Members";
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
         <Title subTitle='Gallery' title='Some Glimpse'/>
         <Gallery/>
         <About/>
+        <Title subTitle='Members' title='Executive Board 2023/20204'/>
+        <Members/>
+        <Title subTitle='Testimonials' title='What Student Saya'/>
+      <Testimonials/>
       </div>
 
      
---
+++ src/components/Testimonial/Testimonials.jsx
@@ -0,0 +1,113 @@
+import React, { useRef } from "react";
+import "./Testimonials.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import user_1 from "../../assets/user1.png";
+
+export default function Testimonials() {
+    const slider = useRef();
+    let tx = 0;
+
+  const slideForward = () => {
+    if(tx> -50){
+        tx -= 25;
+    }
+    slider.current.style.transform =  `translateX(${tx}%)`;
+  };
+  const slideBackward = () => {
+    if(tx < 0){
+        tx += 25;
+    }
+    slider.current.style.transform =  `translateX(${tx}%)`;
+  };
+
+  return (
+    <div className="testimonails">
+      <FontAwesomeIcon
+        className="back-btn"
+        icon={faChevronLeft}
+        onClick={slideBackward}
+      />
+      <FontAwesomeIcon
+        className="next-btn"
+        icon={faChevronRight}
+        onClick={slideForward}
+      />
+      <div className="slider">
+        <ul ref={slider}>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="user1" />
+                <div>
+                  <h3>John Doe</h3>
+                  <span>BSc.CSIT,Batch 2079</span>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur obcaecati vitae excepturi eligendi, quod maxime a
+                iusto officiis accusamus? Alias ullam animi accusantium qui
+                quibusdam harum suscipit deserunt quae sunt?
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="user1" />
+                <div>
+                  <h3>Bebas Pandey</h3>
+                  <span>BSc.CSIT,Batch 2079</span>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur obcaecati vitae excepturi eligendi, quod maxime a
+                iusto officiis accusamus? Alias ullam animi accusantium qui
+                quibusdam harum suscipit deserunt quae sunt?
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="user1" />
+                <div>
+                  <h3>Ez-snippet</h3>
+                  <span>BSc.CSIT,Batch 2079</span>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur obcaecati vitae excepturi eligendi, quod maxime a
+                iusto officiis accusamus? Alias ullam animi accusantium qui
+                quibusdam harum suscipit deserunt quae sunt?
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="user1" />
+                <div>
+                  <h3>Carry Minati</h3>
+                  <span>BSc.CSIT,Batch 2079</span>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur obcaecati vitae excepturi eligendi, quod maxime a
+                iusto officiis accusamus? Alias ullam animi accusantium qui
+                quibusdam harum suscipit deserunt quae sunt?
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
